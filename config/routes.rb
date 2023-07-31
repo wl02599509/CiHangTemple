7577e8dbeae9af3fa70c8f8abdf990ada0687bf3
Mike <wl02599509@gmail.com>
2023-08-01 00:09:51 +0800

    admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root "home#index"
 
+  namespace 'admin' do
+    get '/home', to: "home#index"
+
+    resources 'managers'
+  end
 end
