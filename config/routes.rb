43b80af5d77c019f667264732cd8e3a634741e2d
Mike <wl02599509@gmail.com>
2023-08-31 22:03:08 +0800

    可註冊與登入

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :managers, class_name: 'Admin::Manager'
-
+  devise_for :managers, class_name: "Admin::Manager", controllers: {
+    sessions: 'devise/managers/sessions',
+    registrations: 'devise/managers/registrations'
+  }
+  # devise_for :managers, controllers: {
+  #   sessions: 'managers/sessions'
+  # }
   root 'home#index'
 
   namespace 'admin' do
     get '/home', to: 'home#index'
-
-    resources 'managers'
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_01_161839) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_31_131957) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "admin_managers", force: :cascade do |t|
-    t.string "id_card_number", default: "", null: false
+    t.string "id_card_number", null: false
+    t.string "email", null: false
+    t.string "name", null: false
+    t.string "address", null: false
+    t.datetime "birth_date", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "name"
-    t.string "email", default: "", null: false
-    t.datetime "birth_date"
-    t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_managers_on_email", unique: true
+    t.index ["id_card_number"], name: "index_admin_managers_on_id_card_number", unique: true
     t.index ["reset_password_token"], name: "index_admin_managers_on_reset_password_token", unique: true
   end
 
